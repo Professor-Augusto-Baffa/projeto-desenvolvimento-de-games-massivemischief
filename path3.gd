4dd262b4ee1c0474b9fd464354c4d90c8dba9ae3
joao_fernandes <joaopedro/jpbf@gmail.com>
2024-05-13 01:58:25 -0300

    fixed files

---
+++ path3.gd
@@ -0,0 +1,15 @@
+extends Path2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var inc=0
+var speed=1000
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	inc+=delta*speed
+	$MobPath3.progress=inc
+	
--- player.gd
+++ player.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 1500.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
-signal hit
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -20,5 +30,4 @@ func _physics_process(delta):
 		var collision = get_slide_collision(i)
 		print("I collided with ", collision.get_collider().name)
 		if collision.get_collider().name == "Mob":
-			emit_signal("hit")
-	
+			hide()
